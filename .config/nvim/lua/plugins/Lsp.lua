2ea220f96a313c8e16505e22095c0bfb2146ff9a
ilia.gavrin <ilia.gavrin@nvi-research.com>
2025-04-18 10:39:38 +0300

    feat: ref

--- .config/nvim/lua/plugins/Lsp.lua
+++ .config/nvim/lua/plugins/Lsp.lua
@@ -1,28 +1,46 @@
 return {
-    {"williamboman/mason.nvim", opts = {}, lazy = true}, {
+    {
+        "williamboman/mason.nvim",
+        opts = {},
+        lazy = true,
+    },
+    {
         "williamboman/mason-lspconfig.nvim",
-        opts = {ensure_installed = {"basedpyright", "marksman"}},
-        lazy = true
-    }, {
+        opts = {
+            ensure_installed = {
+                "basedpyright",
+                "marksman",
+            },
+        },
+        lazy = true,
+    },
+    {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim"
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
         },
         opts = {},
         keys = {
-            {"K", vim.lsp.buf.hover, desc = "LSP Hover"},
-            {"gd", vim.lsp.buf.definition, desc = "LSP Go to Definition"},
-            {"gD", vim.lsp.buf.declaration, desc = "LSP Go to Declaration"},
-            {"grn", vim.lsp.buf.rename, desc = "LSP Rename"}
+            { "K", vim.lsp.buf.hover, desc = "LSP Hover" },
+            { "gd", vim.lsp.buf.definition, desc = "LSP Go to Definition" },
+            { "gD", vim.lsp.buf.declaration, desc = "LSP Go to Declaration" },
+            { "grn", vim.lsp.buf.rename, desc = "LSP Rename" },
         },
         config = function()
-            require("lspconfig").basedpyright.setup({
-                settings = {basedpyright = {typeCheckingMode = "standard"}}
+            local lspconfig = require("lspconfig")
+
+            lspconfig.basedpyright.setup({
+                settings = {
+                    basedpyright = {
+                        typeCheckingMode = "standard",
+                    },
+                },
             })
 
-            require("lspconfig").marksman.setup({})
+            lspconfig.marksman.setup({})
         end,
         lazy = true,
-        ft = {"python", "markdown"},
-    }
+        ft = { "python", "markdown" },
+    },
 }
