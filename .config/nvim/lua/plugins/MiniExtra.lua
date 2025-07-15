8c2ddab13fcad3b6f3621c2abab8eb6be4795ee3
ilia.gavrin <ilia.gavrin@nvi-research.com>
2025-07-15 10:49:02 +0300

    feat: better tmux nav and better nvim struct

--- .config/nvim/lua/plugins/MiniExtra.lua
+++ .config/nvim/lua/plugins/MiniExtra.lua
@@ -3,6 +3,39 @@ return {
 		"echasnovski/mini.extra",
 		opts = {},
 		lazy = true,
-		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>fr",
+				function()
+					require("mini.extra").pickers.lsp({ scope = "references" })
+				end,
+				mode = "n",
+				desc = "Pick References",
+			},
+			{
+				"<leader>fs",
+				function()
+					require("mini.extra").pickers.lsp({ scope = "document_symbol" })
+				end,
+				mode = "n",
+				desc = "Pick Symbols",
+			},
+			{
+				"<leader>f'",
+				function()
+					require("mini.extra").pickers.marks()
+				end,
+				mode = "n",
+				desc = "Pick marks",
+			},
+			{
+				'<leader>f"',
+				function()
+					require("mini.extra").pickers.registers()
+				end,
+				mode = "n",
+				desc = "Pick registers",
+			},
+		},
 	},
 }
