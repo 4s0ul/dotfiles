ae73d1dbece390b8555349c2d10cb7d9479fe6a6
ilia.gavrin <ilia.gavrin@nvi-research.com>
2025-08-26 12:46:33 +0300

    feat: better mini.files logic

--- .config/nvim/lua/plugins/MiniFiles.lua
+++ .config/nvim/lua/plugins/MiniFiles.lua
@@ -1,16 +1,24 @@
 return {
 	{
 		"echasnovski/mini.files",
-		opts = {mappings={go_in='<CR>', go_out='-'}, windows={max_number=1}},
+		opts = { mappings = { go_in = "<CR>", go_out = "-" }, windows = { max_number = 1 } },
 		lazy = false,
 		keys = {
 			{
-				"<leader>e",
+				"<leader>E",
 				function()
 					require("mini.files").open()
 				end,
 				mode = "n",
-				desc = "Explore Files",
+				desc = "Explore Files from cwd",
+			},
+			{
+				"<leader>e",
+				function()
+					require("mini.files").open(vim.api.nvim_buf_get_name(0))
+				end,
+				mode = "n",
+				desc = "Explore Files from current file",
 			},
 		},
 	},
