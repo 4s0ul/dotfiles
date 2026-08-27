c737e4af249f374283caf93f23cbe8fc29c2e787
4s0ul <iliagavrin@gmail.com>
2026-08-27 23:37:37 +0300

    better fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,18 +4,23 @@ fish_config theme choose "catppuccin-mocha"
 # Greeting
 set -g fish_greeting ""
 
-# Nvim as default editor
+# Nvim integration
 set -gx EDITOR nvim
+
+# Eza integration
+alias l="eza --icons=auto"
+alias ll="eza --long --icons=auto --group-directories-first"
+alias la="eza --long --icons=auto --all --group-directories-first"
 
 if status is-interactive
 
-    # Starship prompt
+    # Starship integration
     starship init fish | source
 
-    # FZF default bindings
+    # FZF integration
     fzf --fish | source
 
-    # Zoxide
+    # Zoxide integration
     zoxide init fish | source
 
 end
